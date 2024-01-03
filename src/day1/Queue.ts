863d1c4c53a07884c5d80b6d7d763a31fa2da1dd
p3dxm <ripzkid2k4lore@gmail.com>
2024-01-04 00:26:02 +0700

    quicksort nhu loz deo export duoc

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,9 +1,17 @@
+type Node<T>{
+    value: T,
+    next?: Node<T>,
+}
+
+
 export default class Queue<T> {
     public length: number;
-
+    private head?: Node<T>;
+    private tail?: Node<T>;
     
-
     constructor() {
+        this.head = this.tail = undefined;
+        this.legth = 0;
     }
 
     enqueue(item: T): void {
--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,29 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], low: number, high: number): void{
+    let i = low;
+    let j = high;
+    let pivot = arr.length;
 
+    while(i < j){
+        while(arr[i] < arr[pivot])
+            i++;
+        while(arr[i] > arr[pivot] )
+            j--;
+        if(j = i){
+            let tmp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = tmp;
+        i++;
+        j--;
+        }
+    if (i < high)
+        qs(arr,i,high);
+    if (j > low)  
+        qs(arr,low,j)
+    }
+
+    
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length-1)
 }
